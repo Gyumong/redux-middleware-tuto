109ad168e241f3aa5beb2b99e603ddc359c1ceca
Gyumong <rlaalsrb3559@naver.com>
2021-05-31 23:31:27 +0900

    Add: 프로젝트에 리덕스 적용/counter

---
+++ redux-middleware-tuto/src/modules/counter.js
@@ -0,0 +1,28 @@
+/** @format */
+
+// 액션 타입선언
+
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
+
+// 액션 생성함수
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+// 초기 상태선언
+
+const initialState = 0; //초기 상태가 꼭 객체거나 배열일 필요는 없다. 문자나 숫자도 가능
+
+// 리듀서 작성
+
+export default function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return state + 1;
+    case DECREASE:
+      return state - 1;
+    default:
+      return state;
+  }
+}
